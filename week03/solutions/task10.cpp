5500c26e9769146d3463bba5e1dfa270a7b7224b
Stefan Manov <stefanmanov@users.noreply.github.com>
2023-10-26 01:09:00 +0300

    Added `week03/solutions/task10.cpp`.

---
+++ week03/solutions/task10.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <iostream>
+
+
+int main() {
+  std::cout << "Enter `a`:\t";
+
+  double a = 0.0;
+  std::cin >> a;
+
+  double x = 0.0;
+  if (a > 0) {
+    x = -std::sqrt(std::abs(a - 1)) / (5 * std::pow(a, 1.0 / 3));
+  }
+  else {
+    const double e_to_ax = a * a + 5;
+    x = std::log(e_to_ax) / a;
+  }
+
+  std::cout << "x = " << x << std::endl;
+}
