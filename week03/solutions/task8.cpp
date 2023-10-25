6109cb3fe8e84926eaebc35d4eaa50ea83921fac
Stefan Manov <stefanmanov@users.noreply.github.com>
2023-10-26 01:05:48 +0300

    Added `Create week03/solutions/task8.cpp`.

---
+++ week03/solutions/task8.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Enter a natural number <= 255:\t";
+  unsigned n = 0;
+  std::cin >> n;
+
+  const unsigned n_true_bits = (
+    (n & 1) + ((n >> 1) & 1) + ((n >> 2) & 1) + ((n >> 3) & 1) +
+    ((n >> 4) & 1) + ((n >> 5) & 1) + ((n >> 6) & 1) + ((n >> 7) & 1));
+  std::cout << "The number of 1 bits in " << n << " is " << n_true_bits;
+  std::cout << std::endl;
+}
